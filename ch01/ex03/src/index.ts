3896229a912bcec5966abdd959a594181529b33d
ryoh.aruga <ryoh.aruga@nts.ricoh.co.jp>
2020-02-19 16:18:02 +0900

    [add]ch01/ex03に問題文を追加

--- ch01/ex03/src/index.ts
+++ ch01/ex03/src/index.ts
@@ -1,5 +1,18 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+const isPositive: IsPositiveFunc = num => num >= 0;
 
-console.log(hello("World"));
+// 使用例
+isPositive(5);
+
+// エラー例
+isPositive('foo');
+const res: number = isPositive(123);
+
+/*--
+1-3. 関数の型
+以下のコードで定義される関数isPositiveは、
+数値を受け取ってその数値が0以上ならtrueを、
+0未満ならfalseを返す関数です。
+
+以下のコードに合うように適切な型IsPositiveFunc
+を定義してください。
+*/
