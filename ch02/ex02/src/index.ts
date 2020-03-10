14bf989936f4e2ced23f713a02040595ba26f3b1
ryoh.aruga <ryoh.aruga@nts.ricoh.co.jp>
2020-03-10 11:57:03 +0900

    [add]ch02-ex02問題文追加

---
+++ ch02/ex02/src/index.ts
@@ -0,0 +1,28 @@
+/* 問題文
+2-2. いくつかの文字列を受け取れる関数
+以下のコードで定義されるgetSpeedは、'slow', 'medium', 'fast'
+のいずれかの文字列を受け取って数値を返す関数です。
+この関数に他の文字列を渡すのは型エラーとしたいです。
+この条件を満たすように型Speedを定義してください。
+*/
+
+type Speed = /* ここを入力 */;
+
+function getSpeed(speed: Speed): number {
+  switch (speed) {
+    case "slow":
+      return 10;
+    case "medium":
+      return 50;
+    case "fast":
+      return 200;
+  }
+}
+
+// 使用例
+const slowSpeed = getSpeed("slow");
+const mediumSpeed = getSpeed("medium");
+const fastSpeed = getSpeed("fast");
+
+// エラー例
+getSpeed("veryfast");
